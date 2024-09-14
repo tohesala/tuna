8e5f913a1b64fc3748082acdd1f3c2a9af71efd9
Topi Latva-Salo <tohesala@users.noreply.github.com>
2024-09-14 13:44:33 +0300

    Verify input assumptions in FFT

--- tests/unit/fft_test.py
+++ tests/unit/fft_test.py
@@ -2,12 +2,20 @@ from hypothesis import given, strategies as st
 from tests.base import TestBase
 from tuna.fft import fft
 from tuna.dft import naive_dft
+from tuna.utils import is_power_of_two
 
 
 class TestFFT(TestBase):
-    @given(signal=st.lists(st.integers(min_value=-1e6, max_value=1e6), min_size=1))
+    def test_raises_on_empty_list(self):
+        self.assertRaises(ValueError, fft, [])
+
+    @given(st.lists(st.integers(), min_size=1).filter(lambda x: not is_power_of_two(len(x))))
+    def test_raises_on_lists_with_unsuitable_size(self, signal):
+        self.assertRaises(ValueError, fft, signal)
+
+    @given(st.lists(st.integers(min_value=-1e6, max_value=1e6), min_size=1).filter(lambda x: is_power_of_two(len(x))))
     def test_fft_should_equal_dft(self, signal):
-        self.assertListsAlmostEqual(fft(signal), naive_dft(signal))
+        self.assertListsAlmostEqual(fft(signal), naive_dft(signal), places=5)
 
 
 # class TestInverseFFT(TestBase):
--- tuna/fft.py
+++ tuna/fft.py
@@ -7,32 +7,44 @@ and Tukey in 1965, as discussed in Introduction to Algorithms (CLRS).
 from cmath import exp, pi
 
 from tuna.typing import ComplexSequence, NumericSequence
+from tuna.utils import is_power_of_two
 
 
 def fft(x: NumericSequence) -> ComplexSequence:
     """
     Implements the Fast Fourier Transform (FFT) algorithm. The algorithm follows
-    the variant of Cooley-Tukey that is discussed in Introduction to Algorithms by CLRS.
+    the variant of Cooley-Tukey that is discussed in Introduction to Algorithms
+    by CLRS.
 
-    [Cooley-Tukey FFT algorithm (Wikipedia)](https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm)
+    [Cooley-Tukey FFT algorithm
+    (Wikipedia)](https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm)
 
     Args:
-        x: The input signal as a sequnce of discrete samples.
+        x: The input signal as a sequnce of discrete real valued samples. The
+        list needs to be non empty and have a length that is a power of two.
 
     Returns:
         The complex-valued spectrum of the input signal.
     """
-    n = len(x)
-    if n == 1:
-        return x
-    X = [0] * n
-    even = fft(x[::2])
-    odd = fft(x[1::2])
-    for k in range(n // 2):
-        twiddle_factor = exp(-2j * pi * k / n)
-        X[k] = even[k] + twiddle_factor * odd[k]
-        X[k + n // 2] = even[k] - twiddle_factor * odd[k]
-    return X
+    if len(x) == 0:
+        raise ValueError("Input sequence must not be empty.")
+    if not is_power_of_two(len(x)):
+        raise ValueError("Input length must be a power of two.")
+
+    def _fft(x):
+        n = len(x)
+        if n == 1:
+            return x
+        X = [0] * n
+        even = fft(x[::2])
+        odd = fft(x[1::2])
+        for k in range(n // 2):
+            twiddle_factor = exp(-2j * pi * k / n)
+            X[k] = even[k] + twiddle_factor * odd[k]
+            X[k + n // 2] = even[k] - twiddle_factor * odd[k]
+        return X
+
+    return _fft(x)
 
 
 def ifft(X: ComplexSequence) -> NumericSequence:
