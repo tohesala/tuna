418c50c38d0fb10592666a008b0ee2fcb6491b1e
Topi Latva-Salo <tohesala@users.noreply.github.com>
2024-09-14 21:09:35 +0300

    Implement argmax

--- tests/unit/utils_test.py
+++ tests/unit/utils_test.py
@@ -1,6 +1,6 @@
 from hypothesis import given, strategies as st
 from tests.base import TestBase
-from tuna.utils import is_power_of_two
+from tuna.utils import is_power_of_two, argmax
 
 
 class IsPowerOfTwoTests(TestBase):
@@ -14,3 +14,15 @@ class IsPowerOfTwoTests(TestBase):
     def test_32bit_non_powers_of_two_fail(self, n):
         if n not in self.POWERS_OF_TWO_32:
             self.assertFalse(is_power_of_two(n))
+
+
+class ArgMaxTests(TestBase):
+    def test_argmax_simple_case(self):
+        self.assertEqual(argmax([1, 2, 3, 2, 1]), 2)
+
+    def test_argmax_raises_on_empty_list(self):
+        self.assertRaises(ValueError, argmax, [])
+
+    @given(st.lists(st.integers(), min_size=1))
+    def test_argmax_should_point_to_max_element(self, t):
+        self.assertEqual(t[argmax(t)], sorted(t, reverse=True)[0])
--- tuna/utils.py
+++ tuna/utils.py
@@ -3,6 +3,9 @@ Miscellaneous utility functions.
 """
 
 
+from typing import Sequence
+
+
 def is_power_of_two(n: int) -> bool:
     """
     Check if a number is a power of two.
@@ -16,3 +19,18 @@ def is_power_of_two(n: int) -> bool:
     # any power of two will have exactly one bit set in its binary
     # representation
     return n.is_integer() and int(n) & (int(n) - 1) == 0
+
+
+def argmax(t: Sequence[int]) -> int:
+    """
+    Return the index of the maximum element in a sequence.
+
+    Args:
+        t: The sequence to search.
+
+    Returns:
+        The index of the maximum element in the sequence.
+    """
+    if len(t) == 0:
+        raise ValueError("Can not find the argmax of an empty sequence.")
+    return max(enumerate(t), key=lambda x: x[1])[0]
