6198ca15b91a232451538e11ca6dce531cf6a326
Topi Latva-Salo <tohesala@users.noreply.github.com>
2024-09-25 11:44:01 +0300

    Square wave generator

--- tests/unit/wavegen_test.py
+++ tests/unit/wavegen_test.py
@@ -2,7 +2,7 @@ from hypothesis import given
 from hypothesis import strategies as st
 
 from tests.base import TestBase
-from tuna.wavegen import make_sine_wave
+from tuna.wavegen import make_sine_wave, make_square_wave
 
 
 class MakeSineWaveTests(TestBase):
@@ -41,7 +41,7 @@ class MakeSineWaveTests(TestBase):
 
     @given(st.integers(min_value=1, max_value=2000))
     def test_sine_wave_symmetry_hypothesis(self, f):
-        # a wave, that has an integer frequency, and its reverse should be
+        # a sine wave, that has an integer frequency, and its reverse should be
         # symmetric around 0
         wave = make_sine_wave(f, 4096)
         expected = [-x for x in wave[::-1]]
@@ -52,4 +52,36 @@ class MakeSineWaveTests(TestBase):
         # the sum of the sample magnitudes should be 0 for a sine wave that has
         # an integer frequency
         wave = make_sine_wave(f, 4096)
+        self.assertAlmostEqual(sum(wave), 0)
+
+
+class MakeSquareWaveTests(TestBase):
+    def test_1hz_sampled_at_4hz(self):
+        self.assertListsAlmostEqual(make_square_wave(1, 4), [1, 1, -1, -1])
+
+    def test_1hz_sampled_at_8hz(self):
+        self.assertListsAlmostEqual(
+            make_square_wave(1, 8),
+            [1, 1, 1, 1, -1, -1, -1, -1])
+
+    def test_4hz_sampled_at_16hz(self):
+        self.assertListsAlmostEqual(
+            make_square_wave(4, 16),
+            [1, 1, -1, -1] * 4)
+
+    def test_21hz_sampled_at_16hz(self):
+        self.assertListsAlmostEqual(
+            make_square_wave(4, 16),
+            [1, 1, -1, -1] * 4)
+
+    def test_square_wave_symmetry(self):
+        # a square wave, whose frequency is a rational multiple of the sample
+        # rate, should be symmetric around 0
+        wave = make_square_wave(256, 4096)
+        expected = [-x for x in wave[::-1]]
+        self.assertListsAlmostEqual(wave, expected, places=2)
+
+    @given(st.integers(min_value=1, max_value=2000))
+    def test_square_wave_sum_hypothesis(self, f):
+        wave = make_square_wave(f, 4096)
         self.assertAlmostEqual(sum(wave), 0)
--- tuna/wavegen.py
+++ tuna/wavegen.py
@@ -1,4 +1,4 @@
-from math import pi, sin
+from math import floor, pi, sin
 
 from tuna.types import RealSequence
 
@@ -17,3 +17,19 @@ def make_sine_wave(frequency, sample_rate=4096, duration=1) -> RealSequence:
     """
     t = [i / sample_rate for i in range(int(sample_rate * duration))]
     return [sin(2 * pi * frequency * ti) for ti in t]
+
+
+def make_square_wave(frequency, sample_rate=4096, duration=1) -> RealSequence:
+    """
+    Generate a square wave of a given frequency.
+
+    Args:
+        frequency: The frequency of the square wave in Hz.
+        sample_rate: The sample rate of the signal.
+        duration: The duration of the signal in seconds.
+
+    Returns:
+        The square wave as a sequence of real valued samples.
+    """
+    t = [i / sample_rate for i in range(int(sample_rate * duration))]
+    return [(-1)**floor(2*frequency*ti) for ti in t]
