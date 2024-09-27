102010c06d79cee16ca0192d1f3e8a04f1b741ac
Topi Latva-Salo <tohesala@users.noreply.github.com>
2024-09-27 14:40:10 +0300

    Remove cepstral pitch detection

--- tuna/pitch_detection.py
+++ tuna/pitch_detection.py
@@ -1,13 +1,8 @@
+from math import cos, pi
 
-from math import cos, floor, log2, pi, ulp
-
-from tuna.fft import fft, ifft
+from tuna.fft import fft
 from tuna.types import RealSequence
 from tuna.utils import argmax
-
-# The smallest possible difference between two floating point numbers. This is
-# used to avoid taking the log of zero.
-EPS = ulp(1.0)
 
 
 def hamming_window(n: int) -> RealSequence:
@@ -45,22 +40,3 @@ def detect_pitch_simple(signal: RealSequence, sample_rate: int) -> float:
     # at the first half.
     peak = argmax(mag[:N//2])
     return peak * resolution
-
-
-def detect_pitch_cepstral(signal: RealSequence, sample_rate: int) -> float:
-    """
-    Detects the pitch of a signal using cepstral analysis.
-
-    Args:
-        signal: The input signal as a sequence of discrete real valued samples.
-        sample_rate: The sample rate of the signal.
-
-    Returns:
-        The detected pitch in Hz.
-    """
-    spectrum = fft(signal)
-    log_spectrum = [log2(abs(f + EPS)) for f in spectrum]
-    cepstrum = ifft(log_spectrum)
-    # The peak of the cepstrum (which is a time domain sequence) corresponds to
-    # the period of the fundamental frequency, i.e. the pitch.
-    return sample_rate * argmax(cepstrum) / len(signal)
